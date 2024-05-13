8a75132feb8bd3bdb5f5c66002c7a6a1ad14ee04
Tripp808 <o.ankeli@alustudent.com>
2024-05-13 05:37:04 -0700

    another try

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -7,8 +7,9 @@ def matrix_shape(matrix):
         matrix = matrix[0]
     return shape
 
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1) == [2, 2])
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2) == [2, 3, 5])
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    print(matrix_shape(mat1))
+    mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+            [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+    print(matrix_shape(mat2))
