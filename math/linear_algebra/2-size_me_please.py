e835195320414b2e4599726b24fec9f747ba706c
Tripp808 <o.ankeli@alustudent.com>
2024-05-13 05:49:34 -0700

    documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
+"""Module to determine the shape of a matrix."""
+
+
 def matrix_shape(matrix):
+    """Determine the shape of a matrix.
+
+    Args:
+        matrix (list): A nested list representing a matrix.
+
+    Returns:
+        list: A list containing the dimensions of the matrix.
+
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
+
 
 if __name__ == "__main__":
     mat1 = [[1, 2], [3, 4]]
