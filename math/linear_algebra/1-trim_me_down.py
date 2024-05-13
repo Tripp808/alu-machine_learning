23b00d68b3f9cfe90adc49146b620a8afff6b150
Tripp808 <o.ankeli@alustudent.com>
2024-05-13 04:44:05 -0700

    whitespaces

--- math/linear_algebra/1-trim_me_down.py
+++ math/linear_algebra/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = []  # Initialize the_middle list
-for row in matrix:  # Iterate through each row in matrix
-    the_middle.append([row[2], row[3]])  # Append the 3rd and 4th 
-print("The middle columns of the matrix are: {}".format(the_middle)) 
+matrix=[[1,3,9,4,5,8],[2,4,7,3,4,0],[0,3,4,6,1,5]]
+the_middle=[]#Initializethe_middlelist
+for row in matrix:#Iteratethrougheachrowinmatrix
+the_middle.append([row[2],row[3]])#Appendthe3rdand4th
+print("Themiddlecolumnsofthematrixare:{}".format(the_middle))
