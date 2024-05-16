0dddcc2eb0e338902aeb7d0d5bdb90f71c2e6d17
Tripp808 <o.ankeli@alustudent.com>
2024-05-16 06:05:37 -0700

    fixed

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
-
-
-"""Slice a matrix into specified parts."""
-
-
 import numpy as np
 
-# Define the matrix
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
-
-# Slice the matrix to obtain the middle two rows
-mat1 = matrix[1:3]
-
-# Slice the matrix to obtain the middle two columns
-mat2 = matrix[:, 2:4]
-
-# Slice the matrix to obtain the bottom-right 3x3 matrix
-mat3 = matrix[1:, 3:]
-
-# Display the sliced matrices
+mat1, mat2, mat3 = matrix[1:3], matrix[:, 2:4], matrix[1:, 3:]
 print("The middle two rows of the matrix are:\n{}".format(mat1))
 print("The middle two columns of the matrix are:\n{}".format(mat2))
 print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
