0a8a5df18e25534870747cb754e45f487893681c
Tripp808 <o.ankeli@alustudent.com>
2024-05-16 06:28:22 -0700

    fixed

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 
-""" to calculate the shape of a NumPy array.
-"""
-
-
 def np_shape(matrix):
     """
     Calculates the shape of a NumPy array.
@@ -15,11 +11,11 @@ def np_shape(matrix):
     Returns:
     - tuple of ints: The shape of the input array.
     """
-    shape = tuple(len(matrix) if i == 0 else len(matrix[0]) for i in range(len(matrix.shape)))
+    shape = tuple((len(matrix),) + (len(matrix[0]),) * (len(matrix.shape) - 1))
     return shape
 
 if __name__ == "__main__":
-    mat1 = [1, 2, 3, 4, 5, 6]
+    mat1 = [[1, 2, 3, 4, 5, 6]]
     mat2 = []
     mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
             [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
