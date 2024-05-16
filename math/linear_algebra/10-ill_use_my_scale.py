0b46b99a56ec3539ca03de985386c04312479a48
Tripp808 <o.ankeli@alustudent.com>
2024-05-16 06:42:23 -0700

    changes

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
 
+"""
+This script defines a function np_shape(matrix) that calculates the shape of a numpy.ndarray.
+"""
+
+
 def np_shape(matrix):
     """
-    Calculates the shape of a NumPy array.
+    Calculates the shape of a numpy.ndarray.
 
-    Args:
-    - matrix (numpy.ndarray): Input NumPy array.
+    Parameters:
+    matrix (numpy.ndarray): The input numpy array.
 
     Returns:
-    - tuple of ints: The shape of the input array.
+    tuple: A tuple representing the shape of the input array.
     """
-    shape = tuple((len(matrix),) + (len(matrix[0]),) * (len(matrix.shape) - 1))
-    return shape
+    shapes = {
+        0: lambda m: (),
+        1: lambda m: (len(m),),
+        2: lambda m: (len(m),) * len(m[0]),
+        3: lambda m: (len(m),) * len(m[0]) * len(m[0][0]),
+        # Extend this pattern to handle higher dimensions if needed
+    }
+    return shapes.get(matrix.ndim, lambda m: ()) (matrix)
 
-if __name__ == "__main__":
-    mat1 = [[1, 2, 3, 4, 5, 6]]
-    mat2 = []
-    mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-            [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
+# Sample matrices
+mat1 = [1, 2, 3, 4, 5, 6]
+mat2 = []
+mat3 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+        [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]
 
-    print(np_shape(mat1))
-    print(np_shape(mat2))
-    print(np_shape(mat3))
+# Testing the function
+print(np_shape(mat1))
+print(np_shape(mat2))
+print(np_shape(mat3))
