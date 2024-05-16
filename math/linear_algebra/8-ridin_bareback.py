f97b581ad353ce5f89ac95b60a0ba7c375d235e5
Tripp808 <o.ankeli@alustudent.com>
2024-05-16 03:15:02 -0700

    fix

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,6 @@
 This module defines a function for performing matrix multiplication.
 """
 
-
 def mat_mul(mat1, mat2):
     """
     Perform matrix multiplication.
@@ -23,11 +22,15 @@ def mat_mul(mat1, mat2):
     # Initialize result matrix
     result = []
 
+    # Store the number of columns in mat1
+    mat1_cols = len(mat1[0])
+
     # Perform matrix multiplication
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat2[0])):
-            dot_product = sum(mat1[i][k] * mat2[k][j] for k in range(len(mat1[0])))
+            # Calculate dot product
+            dot_product = sum(mat1[i][k] * mat2[k][j] for k in range(mat1_cols))
             row.append(dot_product)
         result.append(row)
 
@@ -41,4 +44,4 @@ if __name__ == "__main__":
             [5, 6]]
     mat2 = [[1, 2, 3, 4],
             [5, 6, 7, 8]]
-    print(mat_mul(mat1, mat2))  
+    print(mat_mul(mat1, mat2))
