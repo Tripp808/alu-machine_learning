684de0afd24b55c8ee30f5a84b2ba98222afd4a1
Tripp808 <o.ankeli@alustudent.com>
2024-05-17 11:46:28 -0700

    task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 
-import numpy as np
 
-def np_transpose(matrix):
-    return np.transpose(matrix)
+"""
+element wise add, sub, product...
+"""
 
-# Test cases from the main script
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
 
-print(np_transpose(mat1))
-print(mat1)
-print(np_transpose(mat2))
-print(mat2)
-print(np_transpose(mat3))
-print(mat3)
+def np_elementwise(mat1, mat2):
+    """
+    Returns:
+    tuple: Element-wise sum, difference, product, and quotient, respectively.
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
