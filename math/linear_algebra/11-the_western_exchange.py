fccb735292b15761661bf6516492f8eb0f1e1f66
Tripp808 <o.ankeli@alustudent.com>
2024-05-17 08:27:01 -0700

    changed

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -2,27 +2,55 @@
 
 def np_transpose(matrix):
     """
-    Transposes the given matrix.
+    Transpose the given matrix.
 
-    Parameters:
-    matrix: The matrix to be transposed.
+    Args:
+    matrix: A matrix that can be interpreted as a numpy ndarray.
+            It can be a 1D, 2D, or 3D matrix.
 
     Returns:
-    The transposed matrix.
+    Transposed version of the input matrix.
     """
-    return [list(row) for row in zip(*matrix)] or [[]]
+    return matrix.T
 
-# Test cases to demonstrate the function
-mat1 = [[1, 2, 3, 4, 5, 6]]
-mat2 = []
-mat3 = [
-    [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-    [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
-]
+# Example usage (this will run directly when the script is executed):
 
-print(np_transpose(mat1))
-print(mat1)
-print(np_transpose(mat2))
-print(mat2)
-print(np_transpose(mat3))
-print(mat3)
+# Transpose a 1D array
+array_1d = [[1, 2, 3, 4, 5, 6]]
+print("Transpose of 1D array:")
+print(np_transpose(array_1d))
+print()
+
+# Transpose an empty 2D array
+array_2d_empty = [[], []]
+print("Transpose of empty 2D array:")
+print(np_transpose(array_2d_empty))
+print()
+
+# Transpose a 3D array
+array_3d = [[[ 1, 11],
+             [ 6, 16]],
+             
+            [[ 2, 12],
+             [ 7, 17]],
+             
+            [[ 3, 13],
+             [ 8, 18]],
+             
+            [[ 4, 14],
+             [ 9, 19]],
+             
+            [[ 5, 15],
+             [10, 20]]]
+print("Transpose of 3D array:")
+print(np_transpose(array_3d))
+print()
+
+# Transpose another 3D array
+array_3d_another = [[[ 1,  2,  3,  4,  5],
+                     [ 6,  7,  8,  9, 10]],
+
+                    [[11, 12, 13, 14, 15],
+                     [16, 17, 18, 19, 20]]]
+print("Transpose of another 3D array:")
+print(np_transpose(array_3d_another))
