5cf1a0c6b6c84dc29da254b033fc6411331721e1
Tripp808 <o.ankeli@alustudent.com>
2024-05-17 11:54:42 -0700

    run 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+"""
+multiplication of matrices
+"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication.
+    """
+    return np.matmul(mat1, mat2)
