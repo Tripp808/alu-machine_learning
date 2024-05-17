7fc71faab8bef71db1019109e1f3bcc8d85ca500
Tripp808 <o.ankeli@alustudent.com>
2024-05-17 09:54:24 -0700

    function

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,16 +2,12 @@
 
 
 """
-This script defines a function np_shape(matrix) that calculates the shape of a numpy.ndarray
-without using loops, conditionals, or imports.
+This script defines a function np_shape(matrix).
 """
 
 
 def np_shape(matrix):
     """
     Calculates the shape of a numpy.ndarray without using loops, conditionals, or imports.
-
-    Parameters:
-    matrix (numpy.ndarray): The numpy array.
     """
     return getattr(matrix, 'shape', ()) * hasattr(matrix, 'shape')
