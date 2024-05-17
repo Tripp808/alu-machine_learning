cbf4c1f17421d0a34bcd15d924bc7317690c6bbe
Tripp808 <o.ankeli@alustudent.com>
2024-05-17 08:16:08 -0700

    11 fixed

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 
-
-"""transpose of matrix
-"""
-
-import numpy as np
-
-
 def np_transpose(matrix):
-    return np.transpose(matrix)
+    """
+    Transposes the given matrix.
 
-# Test cases from the main script
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+    Parameters:
+    matrix: The matrix to be transposed.
+
+    Returns:
+    The transposed matrix.
+    """
+    return [list(row) for row in zip(*matrix)] or [[]]
+
+# Test cases to demonstrate the function
+mat1 = [[1, 2, 3, 4, 5, 6]]
+mat2 = []
+mat3 = [
+    [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+    [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+]
 
 print(np_transpose(mat1))
 print(mat1)
