0a3ff6814fec36786bff523612800b96745be575
Tripp808 <o.ankeli@alustudent.com>
2024-05-30 00:43:23 -0700

    fix

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,12 +8,9 @@ Module to calculate the summation of squares of the first n natural numbers.
 
 def summation_i_squared(n):
     """
-    Calculate the sum of squares of the first n natural numbers.
-    
+    Calculate the sum of squares of the first n natural numbers.    
     :param n: The number up to which squares are summed.
     :type n: int
-    :return: The sum of squares of the first n natural numbers, or None if n is not valid.
-    :rtype: int or None
     """
     if not isinstance(n, int) or n < 1:
         return None
