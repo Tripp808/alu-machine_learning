08dd9055eaee739864f56304086687f4543865e7
Tripp808 <o.ankeli@alustudent.com>
2024-06-08 07:58:35 -0700

    Poisson Distribution

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -33,6 +33,7 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
 
+
 if __name__ == "__main__":
     # Simulating data manually without using numpy
     data = [5, 4, 6, 6, 3, 7, 5, 6, 4, 5, 5, 6, 4, 4, 5, 5, 5, 5, 4, 6,
